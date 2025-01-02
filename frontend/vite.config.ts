8178c28601ec3f9fd4a66475ba1496e59327fa74
Prabhat Kumar <pkkrsingh877@gmail.com>
2025-01-02 19:49:01 +0530

    Preview working

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+    // Customize further if needed
+  },
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': 'https://api.fragnifique.prabhatkumar.site'
     },
   },
   plugins: [react()],
